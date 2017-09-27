5c1ef98e5089a462be038a53a6853c578bc566fe
bb <bb25028@163.com>
2017-09-27 15:53:59 +0800

    base on smartconfig

---
+++ main/app_test_main.c
@@ -0,0 +1,18 @@
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include "smartconfig.h"
+
+void app_main()
+{
+    // Initialize NVS.
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+	smartconfig_main();
+}
+
---
+++ main/smartconfig.h
@@ -0,0 +1,11 @@
+#ifndef _SMART_CONFIG_H__
+#define _SMART_CONFIG_H__
+
+
+
+
+void smartconfig_main();
+
+
+#endif //_SMART_CONFIG_H__
+
